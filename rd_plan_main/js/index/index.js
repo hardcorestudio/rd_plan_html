1bd94035d2d2698af8be040b31d30781ee43b39f
hellowoody <woodyhello@qq.com>
2018-08-21 12:04:58 +0800

    rd_20180821_1

--- rd_plan_main/js/index/index.js
+++ rd_plan_main/js/index/index.js
@@ -1,1 +1,10 @@
-
+function gologin(){
+    var currentUrl = window.location.href;
+    var url = "";
+    if(currentUrl.indexOf("/index.html")>0){
+        url = currentUrl.split("/index.html")[0] + "/main.html#/login";
+    }else{
+        url = currentUrl + "main.html#/login";
+    }
+    window.location.href = url;
+}
