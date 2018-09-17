6757ba28754aed3ecdcf7e4892d5d877dfef587f
hellowoody <woodyhello@qq.com>
2018-09-17 22:21:37 +0800

    rd_20180917_5

--- rd_plan_main/js/index/index.js
+++ rd_plan_main/js/index/index.js
@@ -1,3 +1,44 @@
+;(function(window,undefined){
+    var explorer = window.navigator.userAgent ;
+    var app=document.getElementById('app');
+    var errorBrowser=document.getElementById('errorBrowser');
+    errorBrowser.style.display = 'none';
+    var flag = true;
+    if (!!window.ActiveXObject || "ActiveXObject" in window) {
+        flag = false
+    }else{
+        //ie
+        if (explorer.indexOf("MSIE") >= 0) {
+            flag = false
+        }
+        //firefox
+        else if (explorer.indexOf("Firefox") >= 0) {
+        }
+        //Chrome
+        else if(explorer.indexOf("Chrome") >= 0){
+        }
+        //Opera
+        else if(explorer.indexOf("Opera") >= 0){
+            flag = false
+        }
+        //Safari
+        else if(explorer.indexOf("Safari") >= 0){
+        }
+        //UCBrowser
+        else if(explorer.indexOf("UCBrowser") >= 0){
+        }
+        else{
+            
+        }
+    }
+    if(!flag){
+        var app=document.getElementById('app');
+        var errorBrowser=document.getElementById('errorBrowser');
+        app.style.display='none';
+        errorBrowser.style.display = 'block';
+    }
+})(window);
+
 function gologin(){
     var currentUrl = window.location.href;
     var url = "";
