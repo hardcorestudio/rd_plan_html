50045d0a4dd0a4e7b22b9b46c4462541bf3ba8df
刘得琛 <>
2018-10-06 23:27:45 +0800

    rd_20181006_2 新页串接口

--- rd_plan_sub/src/components/content/page/environmentalMonitoring.vue
+++ rd_plan_sub/src/components/content/page/environmentalMonitoring.vue
@@ -12,97 +12,214 @@
 	</div>
 </template>
 <script>
-	import Aside from '../Aside.vue';
-	import assTitle from '../../common/assTitle.vue'
-	import assForm from '../../common/assForm.vue'
-	import { checkBrowser } from '../../utils/browserCheck.js'
-	export default {
-		name:'environmentalMonitoring',
-		data(){
-			return {
-				myTitleInfo: {
-					title: "环境监测情况",
-					textInfoList: [
-						"利用处置运行设施监测：利用处置设施如危险废物焚烧设施运行的工艺参数、焚烧残渣热灼减率、活性炭和燃料油等主要原辅材料消耗情况等；",
-						"污染物监测指标及频次：废水、废气的特征污染物和主要污染物，噪声以及监测频率、时间安排等；",
-						"自行监测情况：相应的环境监测仪器、设备，制定监测仪器的维护和标定方案，检测人员应当具备相应的资质；",
-						"委托检测情况：与有检测资质（通过计量认证）的单位签订的委托监测合同，监测时间，监测项目等。"
-					]
-				},
-				userRole: '',
-				title1: {
-					title: "危险废物利用/处置设施运行过程相关参数的检测",
-				},
-				title2: {
-					title: "",
-					subTitle: "利用处置设施运行参数监测情况",
-					placeholder: "必填",
-					text: ""
-				},
-				title3: {
-					title: "",
-					subTitle: "污染物监测指标及频次",
-					placeholder: "必填",
-					text: ""
-				},
-				title4: {
-					title: "",
-					subTitle: "自行监测情况",
-					placeholder: "必填",
-					text: ""
-				},
-				title5: {
-					title: "",
-					subTitle: "委托监测情况",
-					placeholder: "必填",
-					text: ""
-				},
+import Aside from '../Aside.vue';
+import assTitle from '../../common/assTitle.vue'
+import assForm from '../../common/assForm.vue'
+import { checkBrowser, getQueryString } from '../../utils/browserCheck.js'
+import fetch from '../../utils/fetch.js'
+export default {
+	name: 'environmentalMonitoring',
+	data () {
+		return {
+			myTitleInfo: {
+				title: "环境监测情况",
+				textInfoList: [
+					"利用处置运行设施监测：利用处置设施如危险废物焚烧设施运行的工艺参数、焚烧残渣热灼减率、活性炭和燃料油等主要原辅材料消耗情况等；",
+					"污染物监测指标及频次：废水、废气的特征污染物和主要污染物，噪声以及监测频率、时间安排等；",
+					"自行监测情况：相应的环境监测仪器、设备，制定监测仪器的维护和标定方案，检测人员应当具备相应的资质；",
+					"委托检测情况：与有检测资质（通过计量认证）的单位签订的委托监测合同，监测时间，监测项目等。"
+				]
+			},
+			userRole: '',
+			title1: {
+				title: "危险废物利用/处置设施运行过程相关参数的检测",
+			},
+			title2: {
+				title: "",
+				subTitle: "利用处置设施运行参数监测情况",
+				placeholder: "必填",
+				text: ""
+			},
+			title3: {
+				title: "",
+				subTitle: "污染物监测指标及频次",
+				placeholder: "必填",
+				text: ""
+			},
+			title4: {
+				title: "",
+				subTitle: "自行监测情况",
+				placeholder: "必填",
+				text: ""
+			},
+			title5: {
+				title: "",
+				subTitle: "委托监测情况",
+				placeholder: "必填",
+				text: ""
+			},
+		}
+	},
+	components: {
+		'my-aside': Aside,
+		'assTitle': assTitle,
+		'assForm': assForm
+	},
+	mounted () {
+		checkBrowser(() => {
+			// this.$message({
+			// 	showClose: true,
+			// 	message: '您当前使用的浏览器不支持本模块功能，建议使用Chrome浏览器',
+			// 	type: 'warning'
+			// });
+			this.$router.push({ path: '/pageIncompatible' })
+		})
+		this.queryJson = getQueryString()
+
+		fetch({
+			url: '/plan/initHandle',
+			method: 'POST',
+			data: 'params=' + JSON.stringify(this.queryJson)
+		}).then(res => {
+
+			// let res = {
+			// 	"sepaName": "津南区",
+			// 	"WJWT": "czlEcjhPMjRXelI5LzQrVE5JS1hiZjJyRkliYW50UFYrS0lPaXd3aFBsbz0=",
+			// 	"operatorId": "",
+			// 	"status": "",
+			// 	"ROLEID": "CSEP",
+			// 	"empId": "",
+			// 	"ifLogin": "0",
+			// 	"epName": "天津合佳威立雅环境服务有限公司",
+			// 	"epId": "EP201410280910450012",
+			// 	"userType": "CSEP",
+			// 	"newGuideFlag": "",
+			// 	"belongQ": "",
+			// 	"belongSepa": "JNQ",
+			// 	"userPortrait": "",
+			// 	"initEnv": {
+			// 		"ENV_2": "测试2",
+			// 		"ENV_3": "测试3",
+			// 		"sysdate": 1538517015030,
+			// 		"ENV_4": "测试4",
+			// 		"TP_ID": "TP201810020531260014",
+			// 		"STATUS": "00",
+			// 		"ENV_1": "测试1"
+			// 	},
+			// 	"belongS": "",
+			// 	"nickName": "天津合佳威立雅环境服务有限公司",
+			// 	"orgCode": "",
+			// 	"IWBSESSION": "BROWSER-20181002101520",
+			// 	"userId": "EP201410280910450012",
+			// 	"userName": "",
+			// 	"contextPath": "",
+			// 	"realName": "",
+			// 	"orgSeq": ""
+			// }
+			this.userRole = res.userType
+			if (res.initEnv && res.initEnv !== '') {
+				this.title2.text = res.initEnv.ENV_1
+				this.title3.text = res.initEnv.ENV_2
+				this.title4.text = res.initEnv.ENV_3
+				this.title5.text = res.initEnv.ENV_4
 			}
-		},
-		components:{
-			'my-aside':Aside,
-			'assTitle':assTitle,
-			'assForm':assForm
-		},
-		mounted () {
-			checkBrowser(() => {
-				// this.$message({
-				// 	showClose: true,
-				// 	message: '您当前使用的浏览器不支持本模块功能，建议使用Chrome浏览器',
-				// 	type: 'warning'
-				// });
-				this.$router.push({path: '/pageIncompatible'})
+
+		})
+
+	},
+	methods: {
+		doSubmit () {
+			if (this.title2.text === '') {
+				this.$notify.error({
+					title: '警告',
+					message: "请填写利用处置设施运行参数监测情况"
+				});
+				return
+			}
+			if (this.title3.text === '') {
+				this.$notify.error({
+					title: '警告',
+					message: "请填写污染物监测指标及频次"
+				});
+				return
+			}
+			if (this.title4.text === '') {
+				this.$notify.error({
+					title: '警告',
+					message: "请填写自行监测情况"
+				});
+				return
+			}
+			if (this.title5.text === '') {
+				this.$notify.error({
+					title: '警告',
+					message: "请填写委托监测情况"
+				});
+				return
+			}
+
+			const loading = this.$loading({
+				lock: true,
+				text: 'Loading',
+				spinner: 'el-icon-loading',
+				background: 'rgba(0, 0, 0, 0.3)'
+			});
+			let submitData = {}
+			submitData.TP_ID = this.queryJson.TP_ID
+
+			submitData.ENV_1 = this.title2.text
+			submitData.ENV_2 = this.title3.text
+			submitData.ENV_3 = this.title4.text
+			submitData.ENV_4 = this.title5.text
+
+			for (let key in this.queryJson) {
+				submitData[key] = this.queryJson[key]
+			}
+			fetch({
+				url: '',
+				method: 'POST',
+				data: 'params=' + JSON.stringify(submitData)
+			}).then(res => {
+				if (res.resFlag == '0') {
+					this.$notify({
+						title: '成功',
+						message: '保存成功',
+						type: 'success'
+					});
+				} else {
+					this.$notify.error({
+						title: '失败',
+						message: res.resMsg
+					});
+				}
+				loading.close();
 			})
-		},
-		methods: {
-			doSubmit() {
-				console.log("保存save");
-			}
 		}
 	}
+}
 </script>
 <style scoped>
-#environmentalMonitoring{
-	display: -webkit-flex; /* Safari */
-	display: flex;
-	flex:1;
-	flex-direction: row;
-	justify-content:space-between;
-	/*align-items:center;*/
-	align-content:center;
-	overflow-y: hidden;
+#environmentalMonitoring {
+  display: -webkit-flex; /* Safari */
+  display: flex;
+  flex: 1;
+  flex-direction: row;
+  justify-content: space-between;
+  /*align-items:center;*/
+  align-content: center;
+  overflow-y: hidden;
 }
-#environmentalMonitoringContent{
-	flex:1;
-	width: 100%;
-	padding: 40px 5%;
-	height: 100%;
-	box-sizing: border-box;
-	overflow-y: scroll;
+#environmentalMonitoringContent {
+  flex: 1;
+  width: 100%;
+  padding: 40px 5%;
+  height: 100%;
+  box-sizing: border-box;
+  overflow-y: scroll;
 }
-.footerSign{
-	width: 100%;
-	height: 50px;
-	float: left;
+.footerSign {
+  width: 100%;
+  height: 50px;
+  float: left;
 }
 </style>
--- rd_plan_sub/src/components/content/page/lastYearManagePlanRecord.vue
+++ rd_plan_sub/src/components/content/page/lastYearManagePlanRecord.vue
@@ -19,7 +19,9 @@ import Aside from '../Aside.vue';
 import assTitle from '../../common/assTitle.vue'
 import assForm from '../../common/assForm.vue'
 import assSwitch from '../../common/assSwitch.vue'
-import { checkBrowser } from '../../utils/browserCheck.js'
+import { checkBrowser, getQueryString } from '../../utils/browserCheck.js'
+import fetch from '../../utils/fetch.js'
+
 export default {
 	name: 'lastYearManagePlanRecord',
 	data () {
@@ -98,18 +100,145 @@ export default {
 		'assSwitch': assSwitch
 	},
 	mounted () {
-			checkBrowser(() => {
-				// this.$message({
-				// 	showClose: true,
-				// 	message: '您当前使用的浏览器不支持本模块功能，建议使用Chrome浏览器',
-				// 	type: 'warning'
-				// });
-				this.$router.push({path: '/pageIncompatible'})
-			})
-		},
+		checkBrowser(() => {
+			// this.$message({
+			// 	showClose: true,
+			// 	message: '您当前使用的浏览器不支持本模块功能，建议使用Chrome浏览器',
+			// 	type: 'warning'
+			// });
+			this.$router.push({ path: '/pageIncompatible' })
+		})
+		this.queryJson = getQueryString()
+
+		fetch({
+			url: '/plan/initHandle',
+			method: 'POST',
+			data: 'params=' + JSON.stringify(this.queryJson)
+		}).then(res => {
+			// let res = {
+			// 	"sepaName": "津南区",
+			// 	"WJWT": "czlEcjhPMjRXelI5LzQrVE5JS1hiZjJyRkliYW50UFYrS0lPaXd3aFBsbz0=",
+			// 	"operatorId": "",
+			// 	"status": "",
+			// 	"ROLEID": "CSEP",
+			// 	"empId": "",
+			// 	"ifLogin": "0",
+			// 	"epName": "天津合佳威立雅环境服务有限公司",
+			// 	"epId": "EP201410280910450012",
+			// 	"initLastInfo": {
+			// 		"LI_2": "测试2",
+			// 		"TP_ID": "TP201810020531260014",
+			// 		"STATUS": "00",
+			// 		"C_8": 0,
+			// 		"C_9": 0,
+			// 		"C_4": 0,
+			// 		"C_5": 1,
+			// 		"sysdate": 1538517544507,
+			// 		"C_6": 0,
+			// 		"C_7": 1,
+			// 		"C_1": 1,
+			// 		"C_2": 0,
+			// 		"C_3": 1,
+			// 		"LI_1": "测试1"
+			// 	},
+			// 	"userType": "CSEP",
+			// 	"newGuideFlag": "",
+			// 	"belongQ": "",
+			// 	"belongSepa": "JNQ",
+			// 	"userPortrait": "",
+			// 	"belongS": "",
+			// 	"nickName": "天津合佳威立雅环境服务有限公司",
+			// 	"orgCode": "",
+			// 	"IWBSESSION": "BROWSER-20181002101520",
+			// 	"userId": "EP201410280910450012",
+			// 	"userName": "",
+			// 	"contextPath": "",
+			// 	"realName": "",
+			// 	"orgSeq": ""
+			// }
+			if (res.initLastInfo && res.initLastInfo !== '') {
+				this.title1.text = res.initLastInfo.LI_1
+				this.title2.text = res.initLastInfo.LI_2
+
+				this.switchInfo1[0].value = res.initLastInfo.C_1 ? res.initLastInfo.C_1 + "" : '0'
+				this.switchInfo1[1].value = res.initLastInfo.C_2 ? res.initLastInfo.C_2 + "" : '0'
+				this.switchInfo1[2].value = res.initLastInfo.C_3 ? res.initLastInfo.C_3 + "" : '0'
+
+				this.switchInfo2[0].value = res.initLastInfo.C_4 ? res.initLastInfo.C_4 + "" : '0'
+
+				this.switchInfo3[0].value = res.initLastInfo.C_5 ? res.initLastInfo.C_5 + "" : '0'
+				this.switchInfo3[1].value = res.initLastInfo.C_6 ? res.initLastInfo.C_6 + "" : '0'
+
+				this.switchInfo4[0].value = res.initLastInfo.C_7 ? res.initLastInfo.C_7 + "" : '0'
+
+				this.switchInfo5[0].value = res.initLastInfo.C_8 ? res.initLastInfo.C_8 + "" : '0'
+				this.switchInfo5[1].value = res.initLastInfo.C_9 ? res.initLastInfo.C_9 + "" : '0'
+			}
+
+		})
+
+	},
 	methods: {
 		doSubmit () {
-			console.log("保存save");
+			if (this.title1.text === '') {
+				this.$notify.error({
+					title: '警告',
+					message: "请填写上年度各级环保部门检查、环境监测、信息公开情况"
+				});
+				return
+			}
+			if (this.title2.text === '') {
+				this.$notify.error({
+					title: '警告',
+					message: "请填写危险废物比较分析"
+				});
+				return
+			}
+
+			const loading = this.$loading({
+				lock: true,
+				text: 'Loading',
+				spinner: 'el-icon-loading',
+				background: 'rgba(0, 0, 0, 0.3)'
+			});
+			let submitData = {}
+			submitData.TP_ID = this.queryJson.TP_ID
+
+			submitData.LI_1 = this.title1.text
+			submitData.LI_2 = this.title2.text
+
+			submitData.C_1 = this.switchInfo1[0].value + ""
+			submitData.C_2 = this.switchInfo1[1].value + ""
+			submitData.C_3 = this.switchInfo1[2].value + ""
+			submitData.C_4 = this.switchInfo2[0].value + ""
+			submitData.C_5 = this.switchInfo3[0].value + ""
+			submitData.C_6 = this.switchInfo3[1].value + ""
+			submitData.C_7 = this.switchInfo4[0].value + ""
+			submitData.C_8 = this.switchInfo5[0].value + ""
+			submitData.C_9 = this.switchInfo5[1].value + ""
+
+			for (let key in this.queryJson) {
+				submitData[key] = this.queryJson[key]
+			}
+			fetch({
+				url: '',
+				method: 'POST',
+				data: 'params=' + JSON.stringify(submitData)
+			}).then(res => {
+				if (res.resFlag == '0') {
+					this.$notify({
+						title: '成功',
+						message: '保存成功',
+						type: 'success'
+					});
+				} else {
+					this.$notify.error({
+						title: '失败',
+						message: res.resMsg
+					});
+				}
+				loading.close();
+			})
 		}
 	}
 }
@@ -123,7 +252,7 @@ export default {
   justify-content: space-between;
   /*align-items:center;*/
   align-content: center;
-	overflow-y: hidden;
+  overflow-y: hidden;
 }
 #lastYearManagePlanRecordContent {
   flex: 1;
