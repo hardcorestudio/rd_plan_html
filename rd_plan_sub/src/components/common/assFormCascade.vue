82552c4d61d57a1f61ed8c777abd603d7d805bda
刘得琛 <>
2018-11-13 14:23:50 +0800

    rd_20181113_2

--- rd_plan_sub/src/components/common/assFormCascade.vue
+++ rd_plan_sub/src/components/common/assFormCascade.vue
@@ -42,7 +42,7 @@
 									</el-select>
 								</div>
 								<div v-else-if="fItem.type === 'inputWithUnit' || fItem.type === 'inputWithUnitSelect'">
-									<el-input v-model="fItem.text" placeholder="必填(限10位)" type="number" maxlength="10">
+									<el-input v-model="fItem.text" placeholder="必填(限10位)" type="number" maxlength="10" oninput="if(value.length>10)value=value.slice(0,10)">
 										<template slot="append">{{fItem.unit}}</template>
 									</el-input>
 								</div>
